4fb869bfcd3d89f8167584fd149cfbf6801f9a08
Mehdi Naderi Varandi <MehdiNV@hotmail.com>
2025-07-13 19:18:35 +0300

    Create basic React + Tailwind template

---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+function App() {
+  return (
+    <div className="p-4">
+      <h1 className="text-2xl font-bold text-blue-600">React + Tailwind works!</h1>
+    </div>
+  );
+}
+export default App;
